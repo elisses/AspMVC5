787baba72b280222a519da8cf73b2cc0020d4785
Elis Fernanda Agape Santos Vieira <elisvieira@add.addtech.com.br>
2017-01-10 16:34:58 -0200

    Projeto01-Adicionando o EF

---
+++ Projeto01/Contexts/EFContext.cs
@@ -0,0 +1,21 @@
+﻿using Projeto01.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+//Para que nossa aplicação possa se beneficiar do Entity
+//Framework, é preciso que ele acesse a base de dados por meio de um
+//contexto, que representará uma sessão de interação da aplicação
+//com a base de dados, seja para consulta ou atualização.
+namespace Projeto01.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_NEt_MVC_CS") { }
+        
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+        
+    }
+}
---
+++ Projeto01/Controllers/FabricanteController.cs
@@ -0,0 +1,20 @@
+﻿using Projeto01.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto01.Controllers
+{
+    public class FabricanteController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricante
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+    }
+    //pg58
+}
